1dc0af584be4306cea0387529572f8819c23eb1d
Lucas Seijo <lucaseijo7@gmail.com>
2021-08-10 10:53:47 -0300

    validacion de formulario agregada

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
-import API_KEY from './apikey';
+import API_KEY from './apikey.js';
+
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.onload = () => {
+    formulario.addEventListener('submit', validarFormulario);
+}
+
+
+function validarFormulario(e){
+    e.preventDefault();
+
+    const terminoBusqueda = document.querySelector('#termino').value;
+
+    if (terminoBusqueda === ''){
+        mostrarAlerta('Agrega un término de búsqueda')
+
+        return;
+    }
+
+}
+
+function mostrarAlerta(mensaje){
+    const alerta = document.querySelector('.bg-red-100');
+
+    if(!alerta){
+        const alerta = document.createElement('p');
+
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+        alerta.innerHTML = `
+                <strong class="font-bold">Error:</strong>
+                <span class="block sm:inline">${mensaje}</span>
+            `;
+
+        formulario.appendChild(alerta);
+    }
+}
