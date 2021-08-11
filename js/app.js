ce3fb98dfcb01d3f8eb1e93417ab3c2a54f4f68e
Lucas Seijo <lucaseijo7@gmail.com>
2021-08-11 10:52:25 -0300

    se muestran imagenes y formato al realizar busqueda

--- js/app.js
+++ js/app.js
@@ -40,7 +40,7 @@ function mostrarAlerta(mensaje){
 }
 
 function buscarImagenes(termino){
-    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${termino}&per_page=100`;
 
     fetch(url)
         .then(respuesta => respuesta.json())
@@ -51,7 +51,36 @@ function buscarImagenes(termino){
 }
 
 function mostrarImagenes(imagenes){
-    
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // iterar sobre arreglo de imagenes
+
+    imagenes.forEach(imagen => {
+        const {previewURL, likes, views, largeImageURL} = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+
+                    <div class="p-4">
+                        <p class="font-bold"> ${likes} <span class="font-light"> Likes </span> </p>
+                        <p class="font-bold"> ${views} <span class="font-light"> Views </span> </p>
+
+                        <a 
+                        class="block w-full bg-blue-800 hover:bg-blue-500 uppercase text-white font-bold text-center rounded mt-5 p-1"
+                        href="${largeImageURL}" target="_blank" rel="noopener noreferrer">
+                        Ver Imagen
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        `
+    })
+
 }
 
 
